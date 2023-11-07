c5db16bbc8e3a5a90528da31ba462ef70a91364b
dlalstmd <minid0329@gmail.com>
2023-11-08 01:15:54 +0900

    과제 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-            sub1 s = new sub1();
+        sub1 s = new sub1();
+        int[] list = null;
+
+        for(int i=0 ; i<10 ; i++){
+            list[i] = i+1;
+        }
+        s.sort(list);
+        s.print(list);
     }
 }
 
-
 class sub1{
-
+    public void sort(int[] arr){
+        //코드 작성
+    }
+    public void print(int[] arr){
+        for(int i=0 ; i<arr.length ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
