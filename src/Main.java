9ca030d1ff1f0b9be3f758ef354245d8be8077ca
dydwls2227 <113996640+dydwls2227@users.noreply.github.com>
2023-11-14 13:15:00 +0900

    second commit by 박용진 + 선택정렬

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,17 @@ public class Main {
 
 class sub1{
     public void sort(int[] arr){
-        //코드 작성
+        for (int i = 0; i < arr.length - 1; i++) // 배열의 첫번쩨 인덱스부터 마지막 인덱스까지 반복
+            {
+                int minIdx = i; // 비교군중 가장 작은 인덱스를 저장할 변수 선언후 현재 요소인 i로 초기화
+                for (int j = i + 1; j < arr.length; j++) //현재 인덱스 다음부터 배열 끝까지 반복
+                    {
+                        if (arr[j] < arr[minIdx]) // 현재 인덱스의 값보다 작을 시
+                        {
+                            minIdx = j; // 가장 작은 인덱스 저장
+                        }
+                    }
+                swap(arr,i,minIdx); // 최소값과 현재 인덱스의 변경
     }
     public void print(int[] arr){
         for(int i=0 ; i<arr.length ; i++){
